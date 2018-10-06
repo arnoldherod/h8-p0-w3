6ce4b5ff614f7b89e80dfc2d1f666d3f45174271
Arnold <arnold.herod@gmail.com>
2018-10-06 22:38:34 +0700

    Submitting to git and foxhub

--- Exercise-15.js
+++ Exercise-15.js
@@ -1,31 +1,49 @@
+// function groupAnimals(animals) {
+//     // you can only write your code here!
+
+//     animals.sort()
+//     var result = []
+//     var innerRes = [] 
+//     var findLetter = animals[0][0]
+
+//     for (initial = 0; initial < animals.length ; initial++) {
+//         if(animals[initial][0] === findLetter){
+//             innerRes.push(animals[initial])
+//         } else {
+//             result.push(innerRes)
+//             findLetter = animals[initial][0]
+//             innerRes = [animals[initial]]
+//         } 
+//     } result.push(innerRes)
+//     return result
+// }    
+
+// TEST CASES
+  console.log(groupAnimals( ['cacing', 'ayam', 'kuda', 'anoa', 'kancil'] ) );
+  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+//   console.log(groupAnimals( ['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ] ));
+  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+
+  
+  
 function groupAnimals(animals) {
     // you can only write your code here!
 
     var result = []
 
-    for (initial = 0; initial < animals.length; initial++) {
-        var newLetter = []
-        if (initial === 0) {
-            result.push([animals[0]]) // console.log(result) = ['cacing']
-        } else {
-            for (resCounter = 0; resCounter < result.length; resCounter++) {
-                // if huruf pertama dari array[initial]sama, push value ke dalem array dengan huruf sama
-                if (result[resCounter][0][0] === animals[initial][0][0]) {
-                    result[resCounter].push(animals[initial])
-                }
-                // kalau beda
-                // check selanjutnya if ada yang sama ga di dalam array yang huruf sama, kalau ada push
-                // kalau ga ada make new array
-                else {
-                    newLetter.push(animals[initial])
-                }
-            } result.push(newLetter)
-        }
+
+    for (initial = 0 ; initial < animals.length ; initial++){
+        var indikator = "yes"
+        for (inRes = 0 ; inRes < result.length ; inRes++){
+            // console.log(result[inRes][0]) ; break;
+            if(result[inRes][0][0] === animals[initial][0]){
+                result[inRes].push(animals[initial])
+                indikator = "no"
+                break;
+            }
+        } 
+        if(indikator === "yes"){
+            result.push([animals[initial]])
+        } 
     } return result
 }
-
-// TEST CASES
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-//   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-//   // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
